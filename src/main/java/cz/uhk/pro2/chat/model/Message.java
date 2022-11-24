fc5bb1fc288ede62c6f8e82f7d1770b2acd4dcb3
Ales <alesbacul@gmail.com>
2022-11-24 16:58:51 +0100

    Created the Message class

---
+++ src/main/java/cz/uhk/pro2/chat/model/Message.java
@@ -0,0 +1,32 @@
+package cz.uhk.pro2.chat.model;
+
+import java.time.LocalDateTime;
+
+public class Message {
+    private String from, msg;
+    private LocalDateTime time;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+}
